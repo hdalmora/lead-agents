25cdff82e91c4261aeaf4e2b5d881f17fc029cff
Henrique Dal Mora <henrique_dalmora@hotmail.com>
2021-02-25 18:44:40 -0300

    add outsideclickhandler to filters menu button

--- src/components/FilterButton/filter-button.tsx
+++ src/components/FilterButton/filter-button.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 import { observer } from "mobx-react-lite";
 import { IoFilterOutline } from 'react-icons/io5';
 import { FaMapMarkerAlt } from 'react-icons/fa';
@@ -21,48 +22,54 @@ export const FilterButton = () => {
     }
 
     return (
-        <Container isActive={isActive} >
-            
-            <div>
-                <FilterContainer 
-                    isActive={isActive} 
-                    isFilterSelected={leadStore.filterSelected === 'leads'}
-                    onClick={() => {
-                        selectFilter('leads');
-                    }}
-                >
-                    <IconContext.Provider value={{ color: 'var(--blue-accent)', size: '1.7rem' }}>
-                        <FaMapMarkerAlt />
+        <OutsideClickHandler
+            onOutsideClick={() => {
+                setIsActive(false);
+            }}
+        >
+            <Container isActive={isActive} >
+                
+                <div>
+                    <FilterContainer 
+                        isActive={isActive} 
+                        isFilterSelected={leadStore.filterSelected === 'leads'}
+                        onClick={() => {
+                            selectFilter('leads');
+                        }}
+                    >
+                        <IconContext.Provider value={{ color: 'var(--blue-accent)', size: '1.7rem' }}>
+                            <FaMapMarkerAlt />
+                        </IconContext.Provider>
+
+                        <p>
+                            Leads
+                        </p>
+                    </FilterContainer>
+
+                    <FilterContainer 
+                        isActive={isActive} 
+                        isFilterSelected={leadStore.filterSelected === 'clients'}
+                        onClick={() => {
+                            selectFilter('clients');
+                        }}
+                    >
+                        <IconContext.Provider value={{ color: 'var(--green-main-stone)', size: '1.7rem' }}>
+                            <FaMapMarkerAlt />
+                        </IconContext.Provider>
+
+                        <p>
+                            Clientes
+                        </p>
+                    </FilterContainer>
+                </div>
+
+                <ClickFilter onClick={toggleFilterMenu}>
+                    <IconContext.Provider value={{ size: '2rem' }}>
+                        <IoFilterOutline />
                     </IconContext.Provider>
-
-                    <p>
-                        Leads
-                    </p>
-                </FilterContainer>
-
-                <FilterContainer 
-                    isActive={isActive} 
-                    isFilterSelected={leadStore.filterSelected === 'clients'}
-                    onClick={() => {
-                        selectFilter('clients');
-                    }}
-                >
-                    <IconContext.Provider value={{ color: 'var(--green-main-stone)', size: '1.7rem' }}>
-                        <FaMapMarkerAlt />
-                    </IconContext.Provider>
-
-                    <p>
-                        Clientes
-                    </p>
-                </FilterContainer>
-            </div>
-
-            <ClickFilter onClick={toggleFilterMenu}>
-                <IconContext.Provider value={{ size: '2rem' }}>
-                    <IoFilterOutline />
-                </IconContext.Provider>
-            </ClickFilter>
-        </Container>
+                </ClickFilter>
+            </Container>
+        </OutsideClickHandler>
     );
 }
   
